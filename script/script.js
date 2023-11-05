d8a77183333df3530bf7e3e9623a2edd8380bb3f
xionglee01 <74166763+xionglee01@users.noreply.github.com>
2023-11-04 20:58:46 -0700

    Finished assignment
    User can remove cards, by pressing the X button when hovered over the card.

--- script/script.js
+++ script/script.js
@@ -3,27 +3,22 @@ const modal = document.querySelector("#modal");
 const cardContainer = document.querySelector(".cardContainer");
 
 
-function Book(title, author, page, read)
+function Book(index, title, author, page, read)
 {    // constructor
+    this.index = index;
     this.title = title;
     this.author = author;
     this.page = page;
     this.read = read;
 }
 
-//DONT REALLY NEED THIS, JUST HERE FOR TESTING
-function addBookToLibrary(book)
-{
-    myLibrary.push(book);
-    addtoTable(book);
-}
-
 function addtoLibrary()
 {
     const book = document.querySelectorAll("input");
-    let new_book = new Book(book[0].value, book[1].value, book[2].value, book[3].value);
+    let new_book = new Book(myLibrary.length, book[0].value, book[1].value, book[2].value, book[3].checked);
     myLibrary.push(new_book);
     //addtoTable(new_book);
+    console.log(myLibrary);
     createCard(new_book);
 }
 
@@ -33,18 +28,48 @@ function createCard(book)
 {
     const container = document.createElement("div");
     container.className = "con";
+    container.id = book.index;
+    const closed = document.createElement("button");
+    closed.innerText = "X";
+    closed.classList.add("close", "non-visible");
+    closed.addEventListener("click", remove);
+    container.appendChild(closed);
 
-    for(let i = 0; i < Object.keys(book).length; i++)
+    let size = Object.keys(book).length;
+
+    for(let i = 1; i < size; i++)
     {
         const header = document.createElement("h4");
-        header.innerText = Object.keys(book)[i] + ": " + Object.values(book)[i];
+        if(i < size - 1)
+        {
+            header.innerText = Object.keys(book)[i].toUpperCase() + ": " + Object.values(book)[i];
+        }
+        else
+        {
+            header.innerText = Object.keys(book)[i].toUpperCase() + ": ";
+            const button = document.createElement("button");
+            button.addEventListener('click', changeRead);
+            button.id = "readButton";
+            if(book.read === true)
+            {
+                button.className = "read";
+                button.innerText = "YES";
+            }
+            else
+            {
+                button.className = "notread";
+                button.innerText = "NO";
+            }
+            header.appendChild(button);
+        }
         container.appendChild(header);
     }
 
+    container.addEventListener("mouseover", showClose);
+    container.addEventListener("mouseout", showClose);
+
     cardContainer.appendChild(container);
 }
-
-
 
 function addtoTable(book)
 {
@@ -89,4 +114,33 @@ function changeRead()
         this.innerHTML = "YES";
     }
 }
-console.log(myLibrary);
+
+
+function showClose()
+{
+    const button = this.firstElementChild;
+    if(button.classList.contains("non-visible"))
+    {
+        button.classList.replace("non-visible", "visible");
+    }
+    else 
+    {
+        button.classList.replace("visible", "non-visible");
+    }
+}
+
+function remove()
+{
+    const parent = this.parentNode;
+    let book = null;
+    myLibrary.forEach(element => {
+        if(element.index === Number(parent.id))
+        {
+            book = element;
+        }   
+    });
+    let index = myLibrary.indexOf(book);
+    myLibrary.splice(index, 1);
+    console.log(myLibrary);
+    parent.remove();
+}
